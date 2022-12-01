f5140bd79bfcfc583a39b4077a042e37f4b2049d
fadelsaad99 <118170848+fadelsaad99@users.noreply.github.com>
2022-12-01 20:18:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,15 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 #include "lib.h"
-int main () {
 
-bool primo(int num) {
+using namespace std;
 
-    if (num < 1){
-        return false;
-    }else if (num == 2, num == 1) {
-        return true;
+int main() {
+    int x = 0;
+    cin >> x;
+    if(primo(x,x)){
+        cout << "numero primo";
+    }else{
+        cout << "numero non primo";
     }
-    bool primo (int x) {
-        for (int i = 2; i < x; i++) {
-            if (x % i == 0) {
-
-                for (int i = 2; i < num; i++)
-                    if (num % i == 0) {
-                        return false;
-                    }
-            }
-            return true;
-        }
-        return 0;
-    }
+    return 0;
+}
