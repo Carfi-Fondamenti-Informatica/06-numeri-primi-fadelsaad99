8b758786320beada45eceb066bf1c71634dc1baf
fadelsaad99 <118170848+fadelsaad99@users.noreply.github.com>
2022-12-01 19:48:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
-#include <iostream>
+#include<iostream>
+#include "libgen.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int x=0;
+    cin >> x;
+    if (x==1){
+        cout << "numero primo" << endl;
+    }
+    else {
+        cout << "numero non primo" << endl;
+    }
+    return 0;
 }
