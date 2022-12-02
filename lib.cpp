6a40b4206f81121e5125762fbcbf89ecc4bf60c8
fadelsaad99 <118170848+fadelsaad99@users.noreply.github.com>
2022-12-02 18:54:21 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "lib.h"
+bool numprimo(int x, int k){
+    if(x % k == 0){
+        if(x == k){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    numprimo(x, k + 1);
+}
