fbe78784ba109af5db374222a08a063321a29c46
fadelsaad99 <118170848+fadelsaad99@users.noreply.github.com>
2022-12-02 23:56:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include "lib.h"
 using namespace std;
 int main(){
-    int x = 0, y = 2;
-    cin >> x;
-    if(numprimo(x, y) == true){
+    int n = 0, i = 2;
+    cin >> n;
+    if(numprimo(n, i) == true){
         cout << "numero primo";
     }
     else{
